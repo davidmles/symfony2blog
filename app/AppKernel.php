795294906a7ec593cf3f918aeb39133ffe7f4db2
David Morales <david@davidmles.com>
2016-03-22 20:03:31 +0100

    Generating the ModelBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Blog\CoreBundle\CoreBundle(),
+            new Blog\ModelBundle\ModelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Blog/ModelBundle/ModelBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Blog\ModelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ModelBundle extends Bundle
+{
+}
