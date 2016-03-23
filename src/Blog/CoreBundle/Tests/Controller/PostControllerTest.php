c6b9e18ac99e835f6da620b89412b27ac5472e58
David Morales <david@davidmles.com>
2016-03-23 09:22:46 +0100

    Writing the first test

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -4,13 +4,20 @@ namespace Blog\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class PostControllerTest
+ */
 class PostControllerTest extends WebTestCase
 {
+    /**
+     * Tests posts index
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-    }
 
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+    }
 }
