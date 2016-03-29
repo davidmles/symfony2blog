9e5f4f3a00f5cb5813f9c2bc5a0d99794165ed29
David Morales <david@davidmles.com>
2016-03-29 20:15:57 +0200

    Writing a test to check the author posts list

--- src/Blog/CoreBundle/Tests/Controller/AuthorControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/AuthorControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Blog\CoreBundle\Tests\Controller;
 
+use Blog\ModelBundle\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -16,7 +17,14 @@ class AuthorControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/show');
-    }
+        /** @var Author $author */
+        $author = $client->getContainer()->get('doctrine')->getManager()->getRepository('ModelBundle:Author')->findFirst();
+        $authorPostsCount = $author->getPosts()->count();
 
+        $crawler = $client->request('GET', '/author/'.$author->getSlug());
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+
+        $this->assertCount($authorPostsCount, $crawler->filter('h2'), 'There should be '.$authorPostsCount.' posts');
+    }
 }
