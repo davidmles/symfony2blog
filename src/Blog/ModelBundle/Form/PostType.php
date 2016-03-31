8ae079986eb1db4ae8f6bdbd64ae645d5f654d32
David Morales <david@davidmles.com>
2016-03-31 15:29:25 +0200

    Adapting the PostType

--- src/Blog/ModelBundle/Form/PostType.php
+++ src/Blog/ModelBundle/Form/PostType.php
@@ -6,29 +6,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Class PostType
+ */
 class PostType extends AbstractType
 {
     /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
+     * {@inheritDoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
-            ->add('slug')
             ->add('body')
-            ->add('author')
-        ;
+            ->add('author');
     }
-    
+
     /**
-     * @param OptionsResolver $resolver
+     * {@inheritDoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Blog\ModelBundle\Entity\Post'
+            'data_class' => 'Blog\ModelBundle\Entity\Post',
         ));
     }
 }
