9ee3bdd953e1ddabf6184e86f10b8a6cf55ae887
David Morales <david@davidmles.com>
2016-03-31 14:15:02 +0200

    Generating the CRUD

---
+++ src/Blog/AdminBundle/Controller/PostController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Blog\ModelBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Blog\ModelBundle\Entity\Post;
+use Blog\ModelBundle\Form\PostType;
+
+/**
+ * Post controller.
+ *
+ * @Route("/post")
+ */
+class PostController extends Controller
+{
+    /**
+     * Lists all Post entities.
+     *
+     * @Route("/", name="post_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $posts = $em->getRepository('ModelBundle:Post')->findAll();
+
+        return $this->render('post/index.html.twig', array(
+            'posts' => $posts,
+        ));
+    }
+
+    /**
+     * Creates a new Post entity.
+     *
+     * @Route("/new", name="post_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createForm('Blog\ModelBundle\Form\PostType', $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('post_show', array('id' => $post->getId()));
+        }
+
+        return $this->render('post/new.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Post entity.
+     *
+     * @Route("/{id}", name="post_show")
+     * @Method("GET")
+     */
+    public function showAction(Post $post)
+    {
+        $deleteForm = $this->createDeleteForm($post);
+
+        return $this->render('post/show.html.twig', array(
+            'post' => $post,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Post entity.
+     *
+     * @Route("/{id}/edit", name="post_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Post $post)
+    {
+        $deleteForm = $this->createDeleteForm($post);
+        $editForm = $this->createForm('Blog\ModelBundle\Form\PostType', $post);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('post_edit', array('id' => $post->getId()));
+        }
+
+        return $this->render('post/edit.html.twig', array(
+            'post' => $post,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Post entity.
+     *
+     * @Route("/{id}", name="post_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Post $post)
+    {
+        $form = $this->createDeleteForm($post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($post);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('post_index');
+    }
+
+    /**
+     * Creates a form to delete a Post entity.
+     *
+     * @param Post $post The Post entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Post $post)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('post_delete', array('id' => $post->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Blog/ModelBundle/Form/PostType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Blog\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('body')
+            ->add('author')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\ModelBundle\Entity\Post'
+        ));
+    }
+}
