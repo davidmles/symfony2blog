f07cec51a4c4bdc7917ce427511153a2f80f81ad
David Morales <david@davidmles.com>
2016-03-31 16:18:07 +0200

    Adapting the controller

--- src/Blog/AdminBundle/Controller/DefaultController.php
+++ src/Blog/AdminBundle/Controller/DefaultController.php
@@ -4,10 +4,22 @@ namespace Blog\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
+/**
+ * Class DefaultController
+ */
 class DefaultController extends Controller
 {
+    /**
+     * Redirection
+     *
+     * @return RedirectResponse
+     *
+     * @Route("/")
+     */
     public function indexAction()
     {
+        return $this->redirect($this->generateUrl('blog_admin_post_index'));
     }
 }
